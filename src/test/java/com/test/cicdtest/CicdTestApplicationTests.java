bfd03442ad94144281615a42082d3d63687a1c4b
김예진 <a89557400@gmail.com>
2022-12-31 23:43:48 +0900

    test commit

---
+++ src/test/java/com/test/cicdtest/CicdTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.cicdtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
